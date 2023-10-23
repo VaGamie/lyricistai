9ba1e1e896732bde6ecadac964cbe9b27d925147
Justwantauser <105529666+Justwantauser@users.noreply.github.com>
2023-10-22 22:12:52 -0400

    Sucessfully liked the backend and the front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends React.Component{
+  state = {
+    
+  }
+
+  showpage(page){
+    document.querySelectorAll('innerwrap').forEach(screen =>{
+      screen.style.display='none'
+    })
+    document.querySelector(`#${page}`).style.display = 'flex'
+  }
+
+  render(){
+    return (
+      <div className="App">
+
+        <div className="menuebar">
+          <button data-page='search' onClick={this.showpage(dataset.page)} >Search</button>
+          <button data-page='favorite' >Favorites</button>
+          <button data-page='history' >History</button>
+
+        </div>
+
+        <div className="wrapper">
+          <div className='innerwrap' id='search'>
+            <h1>This is the search</h1>
+          </div>
+          <div className='innerwrap' id='favorite'>
+            <h1> My favorites</h1>
+          </div>
+          <div className='innerwrap' id='history'>
+            <h1>My history</h1>
+          </div>
+        {/* <h1>Welcome to Lyricize AI</h1> */}
+        </div>
+
+        
+      </div>
+      
+    );
+  };
+};
 export default App;
